0c215117cd025befa4922ce9d15b5a5112fb979b
Asaf Shakarchi <asaf000@gmail.com>
2012-12-30 02:28:56 +0200

    Range support for intersection and range tests. A default natural ordering comparator, used when no Comparator is specified when creating a range instance.

---
+++ src/main/java/com/vertixtech/antiquity/range/NaturalOrderingComparator.java
@@ -0,0 +1,33 @@
+package com.vertixtech.antiquity.range;
+
+import java.util.Comparator;
+
+/**
+ * A natural ordering comparator.
+ * 
+ * Used when no {@link Comparator} is specified when instantiating a {@link RangeTest} object.
+ * 
+ * @see {@link RangeTest}
+ */
+@SuppressWarnings("rawtypes")
+public enum NaturalOrderingComparator implements Comparator {
+	NATURAL;
+
+	/**
+	 * Comparable based compare implementation.
+	 * 
+	 * @param o1
+	 *            the first object to be compared.
+	 * @param o2
+	 *            the second object to be compared.
+	 * @return a negative integer, zero, or a positive integer as the first argument is less than, equal to, or greater
+	 *         than the second.
+	 * 
+	 * @see {@link Comparable#compareTo()}
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public int compare(Object o1, Object o2) {
+		return ((Comparable) o1).compareTo(o2);
+	}
+}
