e86a9cd3333d18381f0eca3ff52ee6ed8416077f
Asaf Shakarchi <asaf000@gmail.com>
2012-12-30 02:38:06 +0200

    Added a test interface for testing Range objects. Added a Range test implementation for Long type.

---
+++ src/test/java/com/vertixtech/antiquity/range/LongRangeTest.java
@@ -0,0 +1,184 @@
+package com.vertixtech.antiquity.range;
+
+import static com.vertixtech.antiquity.range.Range.range;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LongRangeTest implements RangeTest<Long> {
+	private static Range<Long> x = null;
+
+	/**
+	 * Set up the test case.
+	 * 
+	 * Initialize x variable where all tests compares to the 'x' {@link Range}.
+	 */
+	@BeforeClass
+	public static void setUp() {
+		x = range(1L, 10L);
+	}
+
+	@Override
+	@Test
+	public void testRangesEquality() {
+		assertEquals(range(1L, 10L), x);
+		assertEquals(range(10L, 1L), x);
+
+		assertEquals(range(10L, 1L), x.intersectionWith(range(1L, 10L)));
+	}
+
+	@Override
+	@Test
+	public void testIfXContainsPointY() {
+		assertFalse(x.contains(-1L));
+		assertFalse(x.contains(0L));
+
+		for (long l = 1; l <= 10; l++)
+			assertTrue(x.contains(l));
+
+		assertFalse(x.contains(11L));
+		assertFalse(x.contains(12L));
+	}
+
+	@Override
+	@Test
+	public void testIfXIsBeforePointY() {
+		assertFalse(x.isBefore(5L));
+		assertFalse(x.isBefore(1L));
+		assertFalse(x.isBefore(10L));
+		assertTrue(x.isBefore(11L));
+	}
+
+	@Override
+	@Test
+	public void testIfXIsAfterPointY() {
+		assertFalse(x.isAfter(5L));
+		assertFalse(x.isAfter(1L));
+		assertFalse(x.isAfter(10L));
+		assertTrue(x.isAfter(0L));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeYIsContainedWithinX() {
+		Range<Long> y = range(2L, 8L);
+		assertTrue(x.contains(y));
+		assertFalse(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertTrue(x.isOverlappedBy(y));
+
+		assertEquals(y, x.intersectionWith(y));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeYIsConttainedWithinEqualStartX() {
+		Range<Long> y = range(1L, 5L);
+		assertTrue(x.contains(y));
+		assertFalse(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertTrue(x.isOverlappedBy(y));
+
+		assertEquals(y, x.intersectionWith(y));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeYIsContainedWithinEqualEndX() {
+		Range<Long> y = range(4L, 10L);
+		assertTrue(x.contains(y));
+		assertFalse(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertTrue(x.isOverlappedBy(y));
+
+		assertEquals(y, x.intersectionWith(y));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeYIsContainedWitinEqualStartAndEndX() {
+		Range<Long> y = range(1L, 10L);
+		assertTrue(x.contains(y));
+		assertEquals(y, x);
+		assertFalse(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertTrue(x.isOverlappedBy(y));
+
+		assertEquals(y, x.intersectionWith(y));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeNotFullyContainedOverlapsStart() {
+		Range<Long> y = range(-3L, 8L);
+
+		assertFalse(x.contains(y));
+		assertFalse(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertTrue(x.isOverlappedBy(y));
+
+		assertEquals(range(1L, 8L), x.intersectionWith(y));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeNotFullyContainedOverlapsEnd() {
+		Range<Long> y = range(4L, 15L);
+		assertFalse(x.contains(y));
+		assertFalse(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertTrue(x.isOverlappedBy(y));
+
+		assertEquals(range(4L, 10L), x.intersectionWith(y));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeOverlapsEntirePeriod() {
+		Range<Long> y = range(-4L, 15L);
+		assertFalse(x.contains(y));
+		assertFalse(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertTrue(x.isOverlappedBy(y));
+
+		assertEquals(x, x.intersectionWith(y));
+	}
+
+	@Override
+	@Test
+	public void testIfRangeXStartsBeforeY() {
+		Range<Long> y = range(-5L, -2L);
+		assertFalse(x.contains(y));
+		assertFalse(x.isBefore(y));
+		assertTrue(x.isAfter(y));
+		assertFalse(x.isOverlappedBy(y));
+
+		// should throw exception
+		try {
+			x.intersectionWith(y);
+			assertFalse(true);
+		} catch (IllegalArgumentException e) {
+
+		}
+	}
+
+	@Override
+	public void testIfRangeXEndsBeforeY() {
+		Range<Long> y = range(11L, 15L);
+		assertFalse(x.contains(y));
+		assertTrue(x.isBefore(y));
+		assertFalse(x.isAfter(y));
+		assertFalse(x.isOverlappedBy(y));
+
+		// should throw exception
+		try {
+			x.intersectionWith(y);
+			assertFalse(true);
+		} catch (IllegalArgumentException e) {
+
+		}
+	}
+}
---
+++ src/test/java/com/vertixtech/antiquity/range/RangeTest.java
@@ -0,0 +1,255 @@
+package com.vertixtech.antiquity.range;
+
+/**
+ * An interface for testing a {@link Range} of a certain type.
+ * 
+ * @param <T>
+ *            The type elements of the {@link Range}
+ */
+public interface RangeTest<T> {
+	/**
+	 * <p>
+	 * Test whether {@link Range}s are equal
+	 * 
+	 * Element orders is not important (i.e if number is compared than range (1,10) equals (10,1))
+	 * </p>
+	 * 
+	 * @see {@link Range#equals(Object)}
+	 */
+	public void testRangesEquality();
+
+	/**
+	 * <p>
+	 * Test whether range X contains point Y
+	 * </p>
+	 * 
+	 * @see {@link Range#contains(Object)}
+	 */
+	public void testIfXContainsPointY();
+
+	/**
+	 * <p>
+	 * Test whether range X is before point Y
+	 * </p>
+	 * 
+	 * @see {@link Range#isBefore(Object)}
+	 */
+	public void testIfXIsBeforePointY();
+
+	/**
+	 * <p>
+	 * Test whether range X is after point Y
+	 * </p>
+	 * 
+	 * @see {@link Range#isAfter(Object)}
+	 */
+	public void testIfXIsAfterPointY();
+
+/**
+	 * <p>Test if Y {@link Range} is contained with X {@link Range}.</p>
+	 * 
+	 * <p>Where (x.min < y.min) && (x.max > y.max)
+	 * 
+	 * <p>{Expected result of @link Range#contains(Object) is true</p>
+	 * <p>{Expected result of @link Range#isBefore(Object) is false</p>
+	 * <p>{Expected result of @link Range#isAfter(Object) is false</p>
+	 * <p>{Expected result of @link Range#isOverlappedBy(Object) is true</p>
+	 * @see {@link Range#contains(Range)}
+	 * @see {@link Range#isBefore(Range)
+	 * @see {@link Range#isAfter(Range)
+	 */
+	public void testIfRangeYIsContainedWithinX();
+
+	/**
+	 * <p>
+	 * Test if Y {@link Range} is contained within X {@link Range} and start at the same point.
+	 * </p>
+	 * 
+	 * <p>
+	 * Where (x.min == y.min) && (x.max > y.max)
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is true
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is true
+	 * </p>
+	 */
+	public void testIfRangeYIsConttainedWithinEqualStartX();
+
+	/**
+	 * <p>
+	 * Test if Y {@link Range} is contained within X {@link Range} and ends (where x.start == y.start) at the same
+	 * point.
+	 * </p>
+	 * 
+	 * <p>
+	 * Where (x.min >= y.min) && (x.max == y.max)
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is true
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is true
+	 * </p>
+	 * 
+	 * @see {@link Range#contains(Range)}
+	 */
+	public void testIfRangeYIsContainedWithinEqualEndX();
+
+	/**
+	 * <p>
+	 * Test if Y {@link Range} is contained within X {@link Range} and start and ends are equal.
+	 * </p>
+	 * 
+	 * <p>
+	 * This also means that both {@link Range}s objects are equal.
+	 * </p>
+	 * 
+	 * <p>
+	 * Where (x.min == y.min) && (x.end == y.end)
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is true
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is true
+	 * </p>
+	 * 
+	 * @see {@link Range#contains(Range)}
+	 */
+	public void testIfRangeYIsContainedWitinEqualStartAndEndX();
+
+	/**
+	 * <p>
+	 * Test if Y {@link Range} is not fully contained within X {@link Range} and overlaps the start of it.
+	 * </p>
+	 * 
+	 * <p>
+	 * Where (y.min > x.min) && (x.end > y.end)
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is true
+	 * </p>
+	 */
+	public void testIfRangeNotFullyContainedOverlapsStart();
+
+	/**
+	 * <p>
+	 * Test if Y {@link Range} is not fully contained within X {@link Range} and overlaps the end of it.
+	 * </p>
+	 * 
+	 * <p>
+	 * Where (x.min < y.min) && (x.end < y.end)
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is true
+	 * </p>
+	 */
+	public void testIfRangeNotFullyContainedOverlapsEnd();
+
+	/**
+	 * <p>
+	 * Test if Y {@link Range} overlaps X period (in both of its edges)
+	 * </p>
+	 * 
+	 * <p>
+	 * Where (x.min > y.min) && (x.max < y.max)
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is true
+	 * </p>
+	 */
+	public void testIfRangeOverlapsEntirePeriod();
+
+	/**
+	 * <p>
+	 * Test whether X starts before Y
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is true
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is false
+	 * </p>
+	 */
+	public void testIfRangeXStartsBeforeY();
+
+	/**
+	 * <p>
+	 * Test whether X ends before Y
+	 * </p>
+	 * 
+	 * <p>
+	 * {Expected result of @link Range#contains(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isBefore(Object) is true
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isAfter(Object) is false
+	 * </p>
+	 * <p>
+	 * {Expected result of @link Range#isOverlappedBy(Object) is false
+	 * </p>
+	 */
+	public void testIfRangeXEndsBeforeY();
+}
