88eb32f7d3d863fbf823e4fcf179fff527c34b7b
Asaf Shakarchi <asaf000@gmail.com>
2013-01-11 21:14:02 +0200

    A transactional versioned graph implementation.
    Introducing a transactional versioned graph implementation,
    This type of graph capable of versioning all changes that occur within the transaction
    at once.
    
    As opposed to non transactional graph where every single change is associated
    with its own version, in transactional graph, commit versions all modifications
    in the transaction with the same version.
    
    Rollback reverts all transaction changes including versioning changes.

---
+++ src/main/java/com/vertixtech/antiquity/graph/TransactionData.java
@@ -0,0 +1,109 @@
+package com.vertixtech.antiquity.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.tinkerpop.blueprints.Edge;
+import com.tinkerpop.blueprints.Vertex;
+
+/**
+ * A storage class for changes that occur within a transaction.
+ */
+public class TransactionData {
+	private Set<Vertex> addedVertices;
+	private Set<Vertex> removedVertices;
+	private Set<Edge> addedEdges;
+	private Set<Edge> removedEdges;
+	private Map<Vertex, Map<String, Object>> modifiedPropsPerVertex;
+	private Map<Edge, Map<String, Object>> modifiedPropsPerEdge;
+
+	public Set<Vertex> getAddedVertices() {
+		if (addedVertices == null)
+			addedVertices = new HashSet<Vertex>();
+
+		return addedVertices;
+	}
+
+	public void setAddedVertices(Set<Vertex> addedVertices) {
+		this.addedVertices = addedVertices;
+	}
+
+	public Set<Vertex> getRemovedVertices() {
+		if (removedVertices == null)
+			removedVertices = new HashSet<Vertex>();
+
+		return removedVertices;
+	}
+
+	public void setRemovedVertices(Set<Vertex> removedVertices) {
+		this.removedVertices = removedVertices;
+	}
+
+	public Set<Edge> getAddedEdges() {
+		if (addedEdges == null)
+			addedEdges = new HashSet<Edge>();
+
+		return addedEdges;
+	}
+
+	public void setAddedEdges(Set<Edge> addedEdges) {
+		this.addedEdges = addedEdges;
+	}
+
+	public Set<Edge> getRemovedEdges() {
+		if (removedEdges == null)
+			removedEdges = new HashSet<Edge>();
+
+		return removedEdges;
+	}
+
+	public void setRemovedEdges(Set<Edge> removedEdges) {
+		this.removedEdges = removedEdges;
+	}
+
+	public Map<Vertex, Map<String, Object>> getModifiedPropsPerVertex() {
+		if (modifiedPropsPerVertex == null)
+			modifiedPropsPerVertex = new HashMap<Vertex, Map<String, Object>>();
+
+		return modifiedPropsPerVertex;
+	}
+
+	public void setModifiedPropsPerVertex(Map<Vertex, Map<String, Object>> modifiedPropsPerVertex) {
+		this.modifiedPropsPerVertex = modifiedPropsPerVertex;
+	}
+
+	public Map<Edge, Map<String, Object>> getModifiedPropsPerEdge() {
+		if (modifiedPropsPerEdge == null)
+			modifiedPropsPerEdge = new HashMap<Edge, Map<String, Object>>();
+
+		return modifiedPropsPerEdge;
+	}
+
+	public void setModifiedPropsPerEdge(Map<Edge, Map<String, Object>> modifiedPropsPerEdge) {
+		this.modifiedPropsPerEdge = modifiedPropsPerEdge;
+	}
+
+	public void clear() {
+		getAddedVertices().clear();
+		getRemovedVertices().clear();
+		getAddedEdges().clear();
+		getRemovedEdges().clear();
+		getModifiedPropsPerVertex();
+		getModifiedPropsPerEdge();
+	}
+
+	public String getDataAsString() {
+		StringBuffer str = new StringBuffer("Vertices to add:\n");
+
+		for (Vertex v : getAddedVertices())
+			str.append("\t").append(v).append("\n");
+
+		str.append("Vertices to remove:\n");
+		for (Vertex v : getRemovedVertices())
+			str.append("\t").append(v).append("\n");
+
+		return str.toString();
+	}
+}
---
+++ src/test/java/com/vertixtech/antiquity/graph/TransactionalVersionedGraphTest.java
@@ -0,0 +1,72 @@
+package com.vertixtech.antiquity.graph;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.test.ImpermanentGraphDatabase;
+
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
+import com.vertixtech.antiquity.graph.identifierBehavior.LongGraphIdentifierBehavior;
+import com.vertixtech.antiquity.range.Range;
+
+public class TransactionalVersionedGraphTest {
+	private TransactionalVersionedGraph<Neo4jGraph, Long> graph;
+
+	@Before
+	public void setUp() {
+		Neo4jGraph baseGraph = new Neo4jGraph(new ImpermanentGraphDatabase());
+		graph = new TransactionalVersionedGraph<Neo4jGraph, Long>(baseGraph, new LongGraphIdentifierBehavior());
+	}
+
+	@After
+	public void tearDown() {
+		graph.shutdown();
+	}
+
+	@Test
+	public void fewAddAndRemovalOfVerticesInTheSameTransactionShouldHaveTheSameVersion() {
+		Vertex initial = graph.addVertex("vInit");
+		graph.commit();
+		Long ver1 = graph.getLatestGraphVersion();
+		assertNotNull(ver1);
+
+		Vertex v1 = graph.addVertex("v1");
+		Vertex v2 = graph.addVertex("v2");
+		Vertex v3 = graph.addVertex("v3");
+		graph.removeVertex(initial);
+		System.out.println(ElementUtils.getElementPropsAsString(((VersionedVertex<Long>) v1).getBaseVertex(), true));
+		graph.commit();
+		Long ver2 = graph.getLatestGraphVersion();
+
+		assertEquals(Range.range(ver2, graph.getMaxPossibleGraphVersion()), graph.getVersionRange(v1));
+		assertEquals(Range.range(ver2, graph.getMaxPossibleGraphVersion()), graph.getVersionRange(v2));
+		assertEquals(Range.range(ver2, graph.getMaxPossibleGraphVersion()), graph.getVersionRange(v3));
+		assertEquals(Range.range(ver1, ver1), graph.getVersionRange(initial));
+	}
+
+	@Test
+	public void createVerticesAndSetPropertiesShouldHaveTheSameVersion() {
+		Vertex v1 = graph.addVertex("v1");
+		Vertex v2 = graph.addVertex("v2");
+		Vertex v3 = graph.addVertex("v3");
+		v1.setProperty("key1", "foo1");
+		v2.setProperty("key2", "foo2");
+		v3.setProperty("key3", "foo3");
+		graph.commit();
+		Long ver = graph.getLatestGraphVersion();
+
+		// TODO: Consider setting forVersion automatically
+		((VersionedVertex<Long>) v1).setForVersion(ver);
+		((VersionedVertex<Long>) v2).setForVersion(ver);
+		((VersionedVertex<Long>) v3).setForVersion(ver);
+
+		Assert.assertEquals("foo1", v1.getProperty("key1"));
+		Assert.assertEquals("foo2", v2.getProperty("key2"));
+		Assert.assertEquals("foo3", v3.getProperty("key3"));
+	}
+}
