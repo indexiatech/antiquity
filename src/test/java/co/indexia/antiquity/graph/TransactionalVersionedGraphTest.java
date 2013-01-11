969a4e64a0c1e452c6ed2d48353e143cb45b6f0f
Asaf Shakarchi <asaf000@gmail.com>
2013-01-12 01:59:08 +0200

    Refactored tests to be more generic.
    - Refactored VersionedGraphTest to be version type generic.
    - A new non transactional long test (NonTransactionalLongTypeVersionedGraphTest).
    - A new transactional long test (TransactionalVersionedGraphTest).

--- src/test/java/co/indexia/antiquity/graph/TransactionalVersionedGraphTest.java
+++ src/test/java/co/indexia/antiquity/graph/TransactionalVersionedGraphTest.java
@@ -1,37 +1,67 @@
 package co.indexia.antiquity.graph;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.lang.reflect.Method;
+
 import junit.framework.Assert;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.neo4j.test.ImpermanentGraphDatabase;
 
+import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.TestSuite;
+import com.tinkerpop.blueprints.TransactionalGraph;
 import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.blueprints.impls.GraphTest;
 import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph;
 import co.indexia.antiquity.graph.identifierBehavior.LongGraphIdentifierBehavior;
 import co.indexia.antiquity.range.Range;
 
-public class TransactionalVersionedGraphTest {
-	private TransactionalVersionedGraph<Neo4jGraph, Long> graph;
+public class TransactionalVersionedGraphTest extends GraphTest {
+	public TransactionalVersionedGraph<Neo4jGraph, Long> graph;
 
-	@Before
-	public void setUp() {
-		Neo4jGraph baseGraph = new Neo4jGraph(new ImpermanentGraphDatabase());
-		graph = new TransactionalVersionedGraph<Neo4jGraph, Long>(baseGraph, new LongGraphIdentifierBehavior());
+	@Override
+	public Graph generateGraph() {
+		return generateGraph("graph");
 	}
 
-	@After
-	public void tearDown() {
+	@Override
+	public Graph generateGraph(final String graphDirectoryName) {
+		Neo4jGraph baseGraph = new Neo4jGraph(new ImpermanentGraphDatabase());
+		return new TransactionalVersionedGraph<Neo4jGraph, Long>(baseGraph, new LongGraphIdentifierBehavior());
+	}
+
+	@Override
+	public void doTestSuite(TestSuite testSuite) throws Exception {
+		for (Method method : testSuite.getClass().getDeclaredMethods()) {
+			if (method.getName().startsWith("test")) {
+				System.out.println("Testing " + method.getName() + "...");
+				method.invoke(testSuite);
+			}
+		}
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		graph = (TransactionalVersionedGraph<Neo4jGraph, Long>) generateGraph(null);
+	}
+
+	@Override
+	public void tearDown() throws Exception {
 		graph.shutdown();
 	}
 
-	@Test
-	public void fewAddAndRemovalOfVerticesInTheSameTransactionShouldHaveTheSameVersion() {
-		Vertex initial = graph.addVertex("vInit");
+	public void commit() {
 		graph.commit();
+	}
+
+	/*
+	 * TODO: Not working yet as VersionedGraphTestSuite requires to commit every change public void
+	 * testVersionedGraphTestSuite() throws Exception { this.stopWatch(); doTestSuite(new
+	 * VersionedGraphTestSuite<Long>(this)); printTestPerformance("GraphTestSuite", this.stopWatch()); }
+	 */
+
+	public void testFewAddAndRemovalOfVerticesInTheSameTransactionShouldHaveTheSameVersion() {
+		Vertex initial = graph.addVertex("vInit");
+		((TransactionalGraph) graph).commit();
 		Long ver1 = graph.getLatestGraphVersion();
 		assertNotNull(ver1);
 
@@ -40,7 +70,7 @@ public class TransactionalVersionedGraphTest {
 		Vertex v3 = graph.addVertex("v3");
 		graph.removeVertex(initial);
 		System.out.println(ElementUtils.getElementPropsAsString(((VersionedVertex<Long>) v1).getBaseVertex(), true));
-		graph.commit();
+		((TransactionalGraph) graph).commit();
 		Long ver2 = graph.getLatestGraphVersion();
 
 		assertEquals(Range.range(ver2, graph.getMaxPossibleGraphVersion()), graph.getVersionRange(v1));
@@ -49,15 +79,14 @@ public class TransactionalVersionedGraphTest {
 		assertEquals(Range.range(ver1, ver1), graph.getVersionRange(initial));
 	}
 
-	@Test
-	public void createVerticesAndSetPropertiesShouldHaveTheSameVersion() {
+	public void testCreateVerticesAndSetPropertiesShouldHaveTheSameVersion() {
 		Vertex v1 = graph.addVertex("v1");
 		Vertex v2 = graph.addVertex("v2");
 		Vertex v3 = graph.addVertex("v3");
 		v1.setProperty("key1", "foo1");
 		v2.setProperty("key2", "foo2");
 		v3.setProperty("key3", "foo3");
-		graph.commit();
+		((TransactionalGraph) graph).commit();
 		Long ver = graph.getLatestGraphVersion();
 
 		// TODO: Consider setting forVersion automatically
