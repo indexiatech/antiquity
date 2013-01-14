ccfd48dc9140e885b203f12f76300ac31654029e
Asaf Shakarchi <asaf000@gmail.com>
2013-01-14 19:28:00 +0200

    Javadocs for NotFoundException class.

--- src/main/java/co/indexia/antiquity/graph/NotFoundException.java
+++ src/main/java/co/indexia/antiquity/graph/NotFoundException.java
@@ -18,6 +18,13 @@
  */
 package co.indexia.antiquity.graph;
 
+import co.indexia.antiquity.utils.ExceptionFactory;
+
+/**
+ * An exception that is thrown if an element does not exist.
+ * 
+ * @see ExceptionFactory#notFoundException(String)
+ */
 public class NotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
