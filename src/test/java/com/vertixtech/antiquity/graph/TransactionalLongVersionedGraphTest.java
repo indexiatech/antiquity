36961ec5ee4881973753ba0f21c40ecebd799f21
Asaf Shakarchi <asaf000@gmail.com>
2013-01-15 11:47:20 +0200

    Test to assure only 1 vertex is created when adding+modifying a new vertex.
    This test is relevant only for transactional graphs.

--- src/test/java/com/vertixtech/antiquity/graph/TransactionalLongVersionedGraphTest.java
+++ src/test/java/com/vertixtech/antiquity/graph/TransactionalLongVersionedGraphTest.java
@@ -1,6 +1,25 @@
+/**
+ * Copyright (c) 2012-2013 "Vertix Technologies, ltd."
+ *
+ * This file is part of Antiquity.
+ *
+ * Antiquity is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.vertixtech.antiquity.graph;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import junit.framework.Assert;
 
@@ -117,5 +136,21 @@ public class TransactionalLongVersionedGraphTest extends GraphTest {
 		graph.commit();
 		assertTrue(fooV.getPropertyKeys().size() > 0);
 		assertEquals("foo", fooV.getProperty("key"));
+	}
+
+	public void testCreateAndSetOnSameVertexShouldCreateOnlyVertexOnly() {
+		VersionedVertex<Long> fooV = (VersionedVertex<Long>) graph.addVertex("fooV");
+		// Base as currently transient vertex cannot be queried
+		assertNull(fooV.getBaseElement().getProperty("key1"));
+		assertNull(fooV.getBaseElement().getProperty("key2"));
+		fooV.setProperty("key1", "foo");
+		assertEquals("foo", fooV.getBaseElement().getProperty("key1"));
+		fooV.setProperty("key2", "bar");
+		assertEquals("foo", fooV.getBaseElement().getProperty("key1"));
+		assertEquals("bar", fooV.getBaseElement().getProperty("key2"));
+		graph.commit();
+		ArrayList<Vertex> vertexChain = new ArrayList<Vertex>();
+		VersionedGraph.getVertexChain(vertexChain, fooV);
+		assertEquals(1, vertexChain.size());
 	}
 }
